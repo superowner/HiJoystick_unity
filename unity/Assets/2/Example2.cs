6ab960c33fd66b1f1998480dead204ded9806031
Hiram <hriamtan@qq.com>
2017-08-09 14:14:42 +0800

    添加示例

---
+++ unity/Assets/2/Example2.cs
@@ -0,0 +1,25 @@
+﻿//****************************************************************************
+// Description:
+// Author: hiramtan@live.com
+//****************************************************************************
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnValueChange(Vector3 param)
+    {
+        Debug.Log(param);
+    }
+}
---
+++ unity/Assets/3/readme.txt
@@ -0,0 +1,4 @@
+参考资料:
+http://www.xuanyusong.com/archives/3924
+
+将拖拽区域修改成圆形.但是仍旧有问题,就是当拖拽到屏幕边缘的时候
